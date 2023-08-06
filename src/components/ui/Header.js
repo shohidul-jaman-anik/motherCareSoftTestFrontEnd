4e0241e2b6bc9b495e0fde33f61b27f6d66ad26d
Md Anik Hossain <anikh499@gmail.com>
2023-08-06 15:48:05 +0600

    Banner add done

---
+++ src/components/ui/Header.js
@@ -0,0 +1,93 @@
+import Link from 'next/link';
+
+const Header = () => {
+    return (
+        <nav className="bg-gray-800">
+            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                <div className="relative flex items-center justify-between h-16">
+                    <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                        <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                            <span className="sr-only">Open main menu</span>
+                            <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                            </svg>
+
+                            <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+
+
+                    <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                        <div className="hidden sm:block sm:ml-6">
+
+                            <div className="flex space-x-4">
+                                <Link href='/'>
+                                    <h1 className="text-white text-2xl font-semibold" >Mother Care</h1>
+                                </Link>
+                                <Link href="/" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</Link>
+
+                                <Link href="/addApi" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Dashboard</Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Notification */}
+
+                    <div className="dropdown dropdown-end">
+                        <label tabIndex={0} className=" ">
+                            <div className="indicator">
+                                <svg className="h-8 w-8 bg-white rounded-full" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                                </svg>
+                                {/* <span className="badge badge-sm rounded-full indicator-item text-2xl text-red-500">{notification.noti}</span> */}
+                            </div>
+                        </label>
+                        <div tabIndex={0} className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
+                            {/* <div className="card-body">
+                                <span className="font-bold text-lg text-info">{notification.details} </span>
+                                <span className="">{notification.title}</span>
+                            </div> */}
+                        </div>
+                    </div>
+
+
+                    <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+
+
+                        <div className="dropdown dropdown-end">
+                            <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 rounded-full">
+                                    {/* <img src={profile} alt='' /> */}
+                                </div>
+                            </label>
+                            <ul tabIndex="0" className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                                <li>
+                                    <Link href="/updateProfile"
+                                        className="justify-between">
+                                        Update Profile
+                                        <span className="badge">New</span>
+                                    </Link>
+                                </li>
+                                {/* <li><Link href="" onClick={handleLogOut}>Logout</Link></li> */}
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="sm:hidden" id="mobile-menu">
+                <div className="px-2 pt-2 pb-3 space-y-1">
+                    <Link href="/" className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Home</Link>
+
+                    <Link href="addApi" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Add Data</Link>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
